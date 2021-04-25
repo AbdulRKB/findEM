bf98505a3f09efb16f863ca1af32781a48be8a93
Titus <82715493+CyberTitus@users.noreply.github.com>
2021-04-25 13:30:31 +0100

    main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import nmap3
 import netifaces as ni
+from rich.console import Console
+
+console = Console()
 nmap = nmap3.NmapHostDiscovery()
 #Below is what is deciding what to do in different cases, then its doing the scan
 try:
@@ -18,15 +21,17 @@ except:
         print("\nGood Bye!")
 
 
+console.print(f'[yellow]MAC ADDRESS       [blue]---   [yellow]DEVICE NAME', style="bold")
 #Below is what's printing the result to the user
 try:
     for result in results:
         try:
             mac = results[result]['macaddress']['addr']
             name = results[result]['macaddress']['vendor']
-            print(F'{mac}       ||       {name}')
+            console.print(f'[yellow]{mac} [blue] --- [yellow]{name}', style="bold")
         except:
             pass
 
 except NameError:
     pass
+
